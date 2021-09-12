051d75cd1d3c2e97772301c0c71f4e1f990b1f2f
Muhammad Hamza Amir <muhammadhamza2084@gmail.com>
2021-09-12 22:20:55 +0500

    applied callback interface and function to get data from FirebaseCallback

--- app/src/main/java/com/example/budget/MainActivity.java
+++ app/src/main/java/com/example/budget/MainActivity.java
@@ -3,15 +3,67 @@ package com.example.budget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public backend value= new backend();
+    public Button set_bug ;
+    public Button set_days;
+    public Button sp_td;
+    public Button day_min;
+    public Button calc;
+
+    public TextView budget_text;
+    public TextView days_text;
+    public TextView perday_text;
+
+    public EditText budget_input;
+    public EditText days_input;
+    public EditText sp_td_input;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //elements
+        //buttons
+        set_bug = findViewById(R.id.budget_set_button);
+        set_days = findViewById(R.id.days_set_button);
+        sp_td = findViewById(R.id.spend_set_button);
+        day_min = findViewById(R.id.days_minus);
+        calc = findViewById(R.id.calculate);
+
+        //TextView
+        budget_text = findViewById(R.id.budget_text);
+        days_text = findViewById(R.id.days_text);
+        perday_text = findViewById(R.id.text_result);
+
+        //edittext
+        budget_input = findViewById(R.id.budget_input);
+        days_input = findViewById(R.id.days_input);
+        sp_td_input = findViewById(R.id.spent_input);
+
+
+
+        value.getBudget(new FirebaseCallback() {
+            @Override
+            public void onCallback(double value) {
+                budget_text.setText("Budget: "+value);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/budget/backend.java
+++ app/src/main/java/com/example/budget/backend.java
@@ -3,14 +3,34 @@ package com.example.budget;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.Application;
+import android.os.Handler;
 import android.util.Log;
+import android.util.TimeUtils;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-public class backend {
+import com.google.firebase.functions.FirebaseFunctions;
+
+import java.util.concurrent.TimeUnit;
+
+public class backend extends Application {
+
+    private FirebaseFunctions mFunctions;
+
+    //private FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+    @Override
+    public void onCreate(){
+        //FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/").setPersistenceEnabled(true);
+        //database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+        //database.setPersistenceEnabled(true);
+
+        super.onCreate();
+    }
+
 
     public double calculate_budget(double days,double budget){
         double per_day = budget / days;
@@ -25,18 +45,104 @@ public class backend {
         return (budget-sp_today);
     }
 
-    public double reading_budget(){
+//    private double reading_budget(){
+//        Log.w(TAG, "START OF FUNC");
+//        final String[] budget_val = new String[1];
+//        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+//        String vals = database.getReference("budget").toString();
+//        Log.w("daabse",vals);
+//        DatabaseReference myRef = database.getReference().child("budget");
+//
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                if (dataSnapshot.exists()){
+//                    Log.w(TAG,"OK man yes");
+//                }else {                   Log.w(TAG,"OK man no");
+//                }
+//                String value = dataSnapshot.getValue().toString();
+//                budget_val[0] = value;
+//                Log.w(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        double r_B_val = -1;
+//
+//        r_B_val = Double.parseDouble(budget_val[0]);
+//        return r_B_val;
+//    }
+
+//    public double reading_days(){
+//        final String[] budget_val = new String[1];
+//        final double[] r_B_val = new double[1];
+//        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+//        DatabaseReference myRef = database.getReference().child("days");
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue().toString();
+//                budget_val[0] = value;
+//                r_B_val[0] = Double.parseDouble(budget_val[0]);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//        return r_B_val[0];
+//    }
+
+
+      public void setBudget(double budget){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference().child("budget");
+        //String bud_val = String.valueOf(budget);
+        myRef.setValue(budget);
+    }
+    public void setDays(double days){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference().child("days");
+//        String bud_val = String.valueOf(days);
+        myRef.setValue(days);
+    }
+
+    public void getBudget(FirebaseCallback firebaseCallback){
+        Log.w(TAG, "START OF FUNC");
         final String[] budget_val = new String[1];
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("budget");
+        final double[] r_B_val = new double[1];
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+        String vals = database.getReference("budget").toString();
+        Log.w("daabse",vals);
+        DatabaseReference myRef = database.getReference().child("budget");
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
+                if (dataSnapshot.exists()){
+                    Log.w(TAG,"OK man yes");
+                }else {                   Log.w(TAG,"OK man no");
+                }
+                String value = dataSnapshot.getValue().toString();
                 budget_val[0] = value;
-                Log.d(TAG, "Value is: " + value);
+                r_B_val[0] = Double.parseDouble(budget_val[0]);
+                firebaseCallback.onCallback(r_B_val[0]);
+
+                Log.w(TAG, "Value is: " + value);
             }
 
             @Override
@@ -45,22 +151,33 @@ public class backend {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-        double r_B_val = Double.valueOf(budget_val[0]);
-        return r_B_val;
+
     }
 
-    public double reading_days(){
+    public void getDays(FirebaseCallback firebaseCallback){
+        Log.w(TAG, "START OF FUNC");
         final String[] budget_val = new String[1];
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("days");
+        final double[] r_B_val = new double[1];
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://budget-app-fb18f-default-rtdb.europe-west1.firebasedatabase.app/");
+        String vals = database.getReference("budget").toString();
+        Log.w("daabse",vals);
+        DatabaseReference myRef = database.getReference().child("budget");
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
+                if (dataSnapshot.exists()){
+                    Log.w(TAG,"OK man yes");
+                }else {                   Log.w(TAG,"OK man no");
+                }
+                String value = dataSnapshot.getValue().toString();
                 budget_val[0] = value;
-                Log.d(TAG, "Value is: " + value);
+                r_B_val[0] = Double.parseDouble(budget_val[0]);
+                firebaseCallback.onCallback(r_B_val[0]);
+
+                Log.w(TAG, "Value is: " + value);
             }
 
             @Override
@@ -69,23 +186,13 @@ public class backend {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-        double r_B_val = Double.valueOf(budget_val[0]);
-        return r_B_val;
-    }
 
-    public void writing_budget(double budget){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("budget");
-        String bud_val = String.valueOf(budget);
-        myRef.setValue(bud_val);
-    }
-    public void writing_days(double days){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("days");
-        String bud_val = String.valueOf(days);
-        myRef.setValue(bud_val);
     }
 
 
+}
 
+interface FirebaseCallback{
+
+    void onCallback(double value);
 }
